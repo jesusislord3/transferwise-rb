932a77c29d214568e4a214ec0f0cddac50377199
Jason Crockett <jesusislord3@gmx.co.uk>
2018-09-18 00:52:17 +0100

    Beggineing Money transferwise with skeleton from google and the meny project

---
+++ lib/transferwise/money/rates_store/transferwise_store.rb
@@ -0,0 +1,12 @@
+https://github.com/RubyMoney/money/blob/master/lib/money/rates_store/memory.rb
+
+require 'money'
+require 'money/rates_store/'
+
+# https://github.com/RubyMoney/google_currency/blob/master/lib/money/rates_store/rate_removal_support.rb
+module Money::RatesStore
+  class TransferWise
+
+  end
+end
+
